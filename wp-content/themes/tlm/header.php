9535a2582208c518cb7dee987ddead6f76e76004
Yohann-Moy <yohann.moy@gmail.com>
2025-05-23 12:28:06 +0200

    Réparation de la bétise

--- wp-content/themes/tlm/header.php
+++ wp-content/themes/tlm/header.php
@@ -21,6 +21,7 @@
 </head>
 
 <body <?php body_class(); ?>>
+<h1>TEST</h1>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'tlm' ); ?></a>
